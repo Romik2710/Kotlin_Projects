d13f685d03b30d160bfc81dca681578f29f57a85
Romik2710 <patelromik8@gmail.com>
2022-09-19 09:12:36 +0530

    remove folder

--- Grocery_Test/app/src/main/java/com/example/grocery_test/GroceryRepo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.grocery_test
-
-class GroceryRepo(private val db:GroceryDatabase) {
-    suspend fun insert(items: GroceryItems) = db.getGroceryDao().insert(items)
-    suspend fun delete(items: GroceryItems) = db.getGroceryDao().delete(items)
-
-    fun getAllItems() = db.getGroceryDao().getAllGroceryItems()
-}
