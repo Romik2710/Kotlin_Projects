629086f06e0c4bb0854cc4bcb7addddcd1297293
Romik2710 <patelromik8@gmail.com>
2022-09-28 20:50:57 +0530

    10th commit

--- Grocery App/Grocery_List/app/src/main/java/com/example/grocery_list/DataBase/GroceryRepository.kt
+++ Grocery App/Grocery_List/app/src/main/java/com/example/grocery_list/DataBase/GroceryRepository.kt
@@ -5,7 +5,7 @@ import com.example.grocery_list.UI.GroceryItems
 class GroceryRepository(private val db: GroceryDatabase) {
 
     suspend fun insert(items: GroceryItems) = db.getGroceryDao().insert(items)
-    suspend fun delete(items: GroceryItems) = db.getGroceryDao().insert(items)
+    suspend fun delete(items: GroceryItems) = db.getGroceryDao().delete(items)
 
     fun getAllItems() = db.getGroceryDao().getAllGroceryItems()
 
